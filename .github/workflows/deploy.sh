14ae7aa13001e78ce12433228daead1bd2142b86
Md. Mahadi Islam <mahadiiislam061@gmail.com>
2024-11-19 01:01:04 +0600

    :up: mod cicd

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,19 +1,45 @@
+# #!/bin/bash
+
+# # Navigate to the Laravel application directory
+# cd ~/home/smileaca/evaluation.smileacad.com
+
+# # Run database migrations
+# php artisan migrate --force
+
+# # Clear caches
+# php artisan cache:clear
+
+# # Clear and cache routes
+# php artisan route:cache
+
+# # Clear and cache config
+# php artisan config:cache
+
+# # Clear and cache views
+# php artisan view:cache
+
 #!/bin/bash
 
-# Navigate to the Laravel application directory
-cd ~/home/smileaca/evaluation.smileacad.com
+# Exit immediately if a command exits with a non-zero status
+set -e
 
-# Run database migrations
-php artisan migrate --force
+# FTP server details
+FTP_SERVER=$1
+FTP_USERNAME=$2
+FTP_PASSWORD=$3
+LOCAL_DIR=$4
+REMOTE_DIR=$5
 
-# Clear caches
-php artisan cache:clear
+# Print debug information
+echo "FTP_HOST: $FTP_SERVER"
+echo "FTP_USERNAME: $FTP_USERNAME"
+echo "LOCAL_DIR: $LOCAL_DIR"
+echo "REMOTE_DIR: $REMOTE_DIR"
 
-# Clear and cache routes
-php artisan route:cache
-
-# Clear and cache config
-php artisan config:cache
-
-# Clear and cache views
-php artisan view:cache
+# Use lftp to mirror the local directory to the remote directory, skipping SSL verification
+lftp -d -c "
+set ssl:verify-certificate no;
+open -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER;
+mirror -R --verbose --only-newer --parallel=10 $LOCAL_DIR $REMOTE_DIR;
+bye;
+"
