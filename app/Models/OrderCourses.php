5023a4019d421a86f3c4b1785a046700dfcadaa7
Md. Mahadi Islam <Mahadi@Admins-MacBook-Pro-3.local>
2024-11-23 00:06:49 +0600

    :up: second commit

---
+++ app/Models/OrderCourses.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderCourses extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'order_courses';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'teacher_id',
+        'course_id',
+        'order_id',
+        'status',
+    ];
+
+    public const STATUS_DISABLE = 0;
+    public const STATUS_ENABLE  = 1;
+}
