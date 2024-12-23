89986141272fdb35d8dbd49a94d9682b1935098d
Md. Mahadi Islam <mahadi.islam@wearenext.io>
2024-12-23 23:58:47 +0600

    :up:

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -213,14 +213,13 @@
                 </div>
             </div>
             <div class="row g-5 hanger-line">
-                <!-- Start Single Counter  -->
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                     <div class="rbt-counterup rbt-hover-03 border-bottom-gradient">
                         {{-- <div class="top-circle-shape"></div> --}}
                         <div class="inner">
-                            <div class="rbt-round-icon">
+                            {{-- <div class="rbt-round-icon">
                                 <img src="{!! asset('web/assets/images/icons/counter-01.png') !!}" alt="Icons Images">
-                            </div>
+                            </div> --}}
                             <div class="content">
                                 <h3 class="counter"><span class="odometer" data-count="500">00</span>
                                 </h3>
@@ -229,16 +228,14 @@
                         </div>
                     </div>
                 </div>
-                <!-- End Single Counter  -->
 
-                <!-- Start Single Counter  -->
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 mt--60 mt_md--30 mt_sm--30 mt_mobile--60">
                     <div class="rbt-counterup rbt-hover-03 border-bottom-gradient">
                         {{-- <div class="top-circle-shape"></div> --}}
                         <div class="inner">
-                            <div class="rbt-round-icon">
+                            {{-- <div class="rbt-round-icon">
                                 <img src="{!! asset('web/assets/images/icons/counter-02.png') !!}" alt="Icons Images">
-                            </div>
+                            </div> --}}
                             <div class="content">
                                 <h3 class="counter"><span class="odometer" data-count="800">00</span>
                                 </h3>
@@ -247,16 +244,14 @@
                         </div>
                     </div>
                 </div>
-                <!-- End Single Counter  -->
 
-                <!-- Start Single Counter  -->
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 mt_md--60 mt_sm--60">
                     <div class="rbt-counterup rbt-hover-03 border-bottom-gradient">
                         {{-- <div class="top-circle-shape"></div> --}}
                         <div class="inner">
-                            <div class="rbt-round-icon">
+                            {{-- <div class="rbt-round-icon">
                                 <img src="{!! asset('web/assets/images/icons/counter-03.png') !!}" alt="Icons Images">
-                            </div>
+                            </div> --}}
                             <div class="content">
                                 <h3 class="counter"><span class="odometer" data-count="1000">00</span>
                                 </h3>
@@ -265,16 +260,14 @@
                         </div>
                     </div>
                 </div>
-                <!-- End Single Counter  -->
 
-                <!-- Start Single Counter  -->
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12 mt--60 mt_md--30 mt_sm--30 mt_mobile--60">
                     <div class="rbt-counterup rbt-hover-03 border-bottom-gradient">
                         {{-- <div class="top-circle-shape"></div> --}}
                         <div class="inner">
-                            <div class="rbt-round-icon">
+                            {{-- <div class="rbt-round-icon">
                                 <img src="{!! asset('web/assets/images/icons/counter-04.png') !!}" alt="Icons Images">
-                            </div>
+                            </div> --}}
                             <div class="content">
                                 <h3 class="counter"><span class="odometer" data-count="100">00</span>
                                 </h3>
@@ -283,7 +276,6 @@
                         </div>
                     </div>
                 </div>
-                <!-- End Single Counter  -->
             </div>
         </div>
     </div>
